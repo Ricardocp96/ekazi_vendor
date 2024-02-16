8e687430eb28ef0aba6c09b28052d5a84b8d7326
Ricardocp96 <richardlubanga@outlook.com>
2024-02-17 01:10:11 +0300

    initial home

--- Components/productfeed.js
+++ Components/productfeed.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-rapi-ui';
 import { Ionicons } from '@expo/vector-icons';
 import Constants from 'expo-constants';
+import {Button} from 'react-native-paper';
 export default function productfeed () {
 
   state = {
@@ -22,14 +23,35 @@ export default function productfeed () {
               leftContent={
                 <Ionicons name="search" size={20} color="black"/>
               }
+               style={styles.rounded}
               autoCapitalize="none"
               autoCompleteType="off"
               autoCorrect={false}
               keyboardType="email-address"
        
             />
+
+
+
        </View>
+      
+
+ <View style={styles.buttons}>
+ <View style={styles.sidebyside}>
+ 
+  {/* add the family of buttons here */}
+     
+  
+  <Button icon="camera" mode="elevated"  style={styles.space} onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
+  <Button icon="camera" mode="elevated"  style={styles.space} onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
+  </View>
+  </View>
       </View>
+      
       
     );
   }
@@ -45,11 +67,31 @@ const styles = StyleSheet.create({
 
   SecondContainer:{
     flex: 1,
-    paddingTop: 16 + Constants.statusBarHeight,
+   // paddingTop: 16 + Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     overflowX: 'hidden',
   },
   staticSection: {
     paddingHorizontal: 16,
   },
+
+  experiment:{
+    paddingTop: 16 + Constants.statusBarHeight,
+  },
+buttons:{
+  
+  padding: 10,
+  borderRadius: 5,
+  width: 200, // Adjust the width to make the button smaller
+  alignItems: 'center',
+},
+ sidebyside:{
+  flexDirection: 'row',
+    //justifyContent: 'center', // Center buttons horizontally
+    alignItems: 'center', // Align buttons vertically
+    paddingHorizontal: 5, 
+ },
+ space:{
+  marginHorizontal: 10,
+ },
 });
